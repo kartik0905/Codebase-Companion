a59f33df32106b57820ba783ed777ae52d618f17
Kartik Garg <kartikamitgarg2005@gmail.com>
2025-08-16 14:29:52 +0530

    Bugfixes

--- server.js
+++ server.js
@@ -33,11 +33,24 @@ const requestLogger = (req, res, next) => {
 
 app.use(requestLogger);
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://codebase-companion.vercel.app/",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+   
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
